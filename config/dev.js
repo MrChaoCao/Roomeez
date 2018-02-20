c2163ed61d81160d0ab542da9fedc46d63aae879
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-19 20:13:48 -0800

    Dev.js created

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+// dev.js
+module.exports = {
+ facebookClientID: "409813422792899",
+ facebookClientSecret: "7ee979e2ebcc35e796bb9f7ea6f2d048",
+ mongoURI: 'mongodb://wyatt:password@ds241658.mlab.com:41658/roomeez',
+ cookieKey: 'asd08f7as08g797324asd9g87as8df6a9s65df765', // random keyboard typing.
+};
