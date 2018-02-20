e41fbd38fa825e32f308a01e153e2dd1c3fc5e5c
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-19 20:18:35 -0800

    added dev.js

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// prod keys
+module.exports = {
+ facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+ facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+ mongoURI: process.env.MONGO_URI,
+ cookieKey: process.env.COOKIE_KEY, // random keyboard typing.
+};
