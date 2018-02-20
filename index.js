16ccc266b511bd52e9597da1d83c42dee3187fe5
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-19 19:28:08 -0800

    Got rid of a commented out line. Forgot to checkout a branch

--- index.js
+++ index.js
@@ -51,8 +51,6 @@ app.get('/', (req, res) => {
 });
 
 
-
-// Run local server on port 3000.
 const port = process.env.PORT || 5000;
 const server = app.listen(port, function () {
   console.log('Server running at http://127.0.0.1:' + port + '/');
