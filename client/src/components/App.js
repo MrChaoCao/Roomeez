e1beed4a74d321c03a4a7a1b68ba49cd1c55f387
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-20 18:59:08 -0800

    Frontend done and showing

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      Hey there
+    </div>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Root from './components/root';
+import configureStore from './store/store';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  // if (window.currentUser) {
+  //   const preloadedState = { session: { currentUser: window.currentUser } };
+  //   store = configureStore(preloadedState);
+  //   delete window.currentUser;
+  // } else {
+    store = configureStore();
+  // }
+  window.store = store;
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
+});
