30b81033ed0d86478242a0bbcbefe160402dbd0c
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-21 10:51:22 -0800

    Auth now fully works

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,6 +17,7 @@ module.exports = (app) => {
 
   app.get('/api/logout', (req, res) => {
     req.logout(); // function provided by passport
+    res.redirect('/');
     res.send(req.user);
   });
 
