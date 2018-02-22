6a2eb0d625af5f9e0308e5ba1340c2ce731a744b
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-22 12:00:18 -0800

    hero image and text now display properly

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Landing = () => {
   return (
     <section className="masthead">
-
+      <div className="masthead-image">
+      </div>
+      <div className="masthead-box">
+        <h1>Find Your Next Roommate(s)</h1>
+      </div>
     </section>
   );
 };
