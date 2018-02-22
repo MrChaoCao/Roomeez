e9c5c8749d436af46b91e87d0731e102045e8046
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-21 20:13:01 -0800

    Header links now change if you're logged in

--- client/src/components/Header_container.js
+++ client/src/components/Header_container.js
@@ -2,6 +2,12 @@ import { connect } from 'react-redux';
 import Header from './Header';
 import { fetchUser } from '../actions/user_actions';
 
+const mapStateToProps = state => {
+  return {
+    currentUser: state.currentUser
+  };
+};
+
 const mapDispatchToProps = dispatch => {
   return {
   };
