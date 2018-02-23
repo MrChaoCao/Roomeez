3d7b25ce6123083feb68c22ff4097981a73da212
Chao Cao <mailbox.chao.cao@gmail.com>
2018-02-23 11:30:30 -0800

    Commented out groups controller import in Group.js because it was throwing errors

--- routes/groups.js
+++ routes/groups.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Group = require('../models/group');
-const GroupsController = require('../controllers/GroupsController');
+// const GroupsController = require('../controllers/GroupsController');
 
 // const checkAuth = require(‘../middleware/check-auth’);
 
