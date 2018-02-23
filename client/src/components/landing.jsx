efcd2832d7a4920e0d60380347fb1e8e38301473
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-22 18:51:44 -0800

    More css

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -9,10 +9,19 @@ const Landing = () => {
         <div className="masthead-box">
           <h1>Find Your Next Roommate(s)</h1>
         </div>
-        <a href="#">Learn More</a>
+        <a href="#how-it-works">Learn More</a>
       </section>
-      // <section className="">
-      // </section>
+      <section id="how-it-works">
+        <h1>How It Works</h1>
+        <div className="search-for-individuals-box">
+          <i class="fas fa-user-circle"></i>
+          <p>Search for Individuals Who Need a Place</p>
+        </div>
+        <div className="search-for-groups-box">
+          <i class="fas fa-users"></i>
+          <p>Search for Groups That Need Roommates</p>
+        </div>
+      </section>
   </div>
   );
 };
