3af631cc70f4f83f32bdf86ef981f73599bf8d19
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-24 15:21:12 -0800

    Modified user index

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.get('/:userId', function(req, res) {
 });
 
 // User index
-router.get('/findcompatibleusers/:groupId', function(req, res) {
+router.get('', function(req, res) {
   User.find().exec((err, users) => {
     if (err) {
       return res.status(404).send("Not found");
@@ -31,74 +31,85 @@ router.get('/findcompatibleusers/:groupId', function(req, res) {
       return res.json(users);
     }
   });
-  // Find all users in group :groupId
-  // Figure out group dealbreakers
-  // Find users who meet group dealbreakers
-
-  // Grab members of group :groupId from db
-  // console.log("Step 1");
-  // console.log(req.params.groupId);
-  // mongoose.model('Group').findById(req.params.groupId).select('members').exec((err1, group) => {
-  //   if (err1) {
-  //     return res.status(404).send("Not found");
-  //   } else {
-  //     console.log("Step 2");
-  //     console.log(group);
-  //     // Find group's users
-  //     let memberIds = [];
-  //     group.members.forEach((userId) => {
-  //       memberIds.push(ObjectId(userId));
-  //     });
-  //
-  //     // Grab each group member's dealbreakers from db
-  //     User.find({
-  //       '_id': { $in: memberIds }
-  //     }).select('name dealbreakers').exec(function (err2, members) {
-  //       if (err2) {
-  //         return res.status(404).send("Not found");
-  //       } else {
-  //         console.log("Step 3");
-  //         console.log(members);
-  //         // members = {member1, member2, ...}
-  //         // We have grabbed all members dealbreakers
-  //         // Calculate group's composite dealbreakers
-  //
-  //         // let groupDealbreakers = []; // Array of composite dealbreakers that are true
-  //         let groupDealbreakerQuery = {};
-  //
-  //         members.forEach((member) => {
-  //           Object.keys(member.dealbreakers).forEach((key) => {
-  //             // If any group member has a dealbreaker,
-  //             // it becomes dealbreaker for the whole group
-  //             if (member.dealbreakers[key]) {
-  //               groupDealbreakerQuery[key] = false;
-  //             }
-  //           });
-  //         });
-  //         console.log(groupDealbreakerQuery);
-  //
-  //
-  //         // Now that we have the group's composite dealbreakers,
-  //         // search for users that whose values for thingsIDo
-  //         // matches dealbreakerQuery
-  //         console.log("3.5", { 'thingsIDo': groupDealbreakerQuery });
-  //
-  //         User.find({ 'thingsIDo': groupDealbreakerQuery }).exec(
-  //           function(err, users) {
-  //             if (err) {
-  //               return res.status(404).send("Not found");
-  //             } else {
-  //               console.log("Step 4");
-  //               console.log(users);
-  //               return res.json(users);
-  //             }
-  //           }
-  //         );
-  //       }
-  //     });
-  //   }
-  // });
 });
+
+// User index filtered by dealbreakers
+// router.get('/findcompatibleusers/:groupId', function(req, res) {
+//   User.find().exec((err, users) => {
+//     if (err) {
+//       return res.status(404).send("Not found");
+//     } else {
+//       return res.json(users);
+//     }
+//   });
+//   Find all users in group :groupId
+//   Figure out group dealbreakers
+//   Find users who meet group dealbreakers
+//
+//   Grab members of group :groupId from db
+//   console.log("Step 1");
+//   console.log(req.params.groupId);
+//   mongoose.model('Group').findById(req.params.groupId).select('members').exec((err1, group) => {
+//     if (err1) {
+//       return res.status(404).send("Not found");
+//     } else {
+//       console.log("Step 2");
+//       console.log(group);
+//       // Find group's users
+//       let memberIds = [];
+//       group.members.forEach((userId) => {
+//         memberIds.push(ObjectId(userId));
+//       });
+//
+//       // Grab each group member's dealbreakers from db
+//       User.find({
+//         '_id': { $in: memberIds }
+//       }).select('name dealbreakers').exec(function (err2, members) {
+//         if (err2) {
+//           return res.status(404).send("Not found");
+//         } else {
+//           console.log("Step 3");
+//           console.log(members);
+//           // members = {member1, member2, ...}
+//           // We have grabbed all members dealbreakers
+//           // Calculate group's composite dealbreakers
+//
+//           // let groupDealbreakers = []; // Array of composite dealbreakers that are true
+//           let groupDealbreakerQuery = {};
+//
+//           members.forEach((member) => {
+//             Object.keys(member.dealbreakers).forEach((key) => {
+//               // If any group member has a dealbreaker,
+//               // it becomes dealbreaker for the whole group
+//               if (member.dealbreakers[key]) {
+//                 groupDealbreakerQuery[key] = false;
+//               }
+//             });
+//           });
+//           console.log(groupDealbreakerQuery);
+//
+//
+//           // Now that we have the group's composite dealbreakers,
+//           // search for users that whose values for thingsIDo
+//           // matches dealbreakerQuery
+//           console.log("3.5", { 'thingsIDo': groupDealbreakerQuery });
+//
+//           User.find({ 'thingsIDo': groupDealbreakerQuery }).exec(
+//             function(err, users) {
+//               if (err) {
+//                 return res.status(404).send("Not found");
+//               } else {
+//                 console.log("Step 4");
+//                 console.log(users);
+//                 return res.json(users);
+//               }
+//             }
+//           );
+//         }
+//       });
+//     }
+//   });
+// });
 //
 //   // This route returns all other users that match a group's survey results.
 //   // This is the user output from the hard filter.
