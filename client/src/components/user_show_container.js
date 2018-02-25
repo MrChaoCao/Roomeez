d1a369a31bfe48e620afcb9803a45bf93d515746
Ashil Farahmand <ashil.farahmand@gmail.com>
2018-02-25 14:54:32 -0800

    Minor corrections to user show container

--- client/src/components/user_show_container.js
+++ client/src/components/user_show_container.js
@@ -1,17 +1,17 @@
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions/user_actions';
 import { withRouter } from 'react-router-dom';
-import UserShow from './user_show'
+import UserShow from './user_show';
 
 const mapStateToProps = (state, ownProps) => {
   return {
     userId: ownProps.match.params.userId,
     user: state.entities.users.shownUser,
-  }
+  };
 };
 
 const mapDispatchToProps = dispatch => ({
-  fetchUser: id => dispatch(fetchUser(id)),
+  fetchUser: id => dispatch(fetchUser(id))
 });
 
 export default withRouter(connect(
