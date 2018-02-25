e3083728022ede3efc8893c857dcf49123e25304
Wyatt Rudnicki <wyattrudnicki@gmail.com>
2018-02-24 21:06:52 -0800

    Introduce group show changes to main files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,6 +5,7 @@ import UserSurveyContainer from './user_survey_container';
 
 import HeaderContainer from './Header_container';
 import DashboardContainer from './dashboard/dashboard_container';
+import GroupShowContainer from './GroupShow_container';
 
 class App extends React.Component {
   componentDidMount() {
@@ -22,6 +23,7 @@ class App extends React.Component {
               <Route exact path="/" component={Landing}/>
               <Route path="/survey" component={UserSurveyContainer}/>
               <Route path="/dashboard" component={DashboardContainer}/>
+              <Route exact path="/groups/:groupId" component={GroupShowContainer}/>
             </div>
           </BrowserRouter>
         </div>
