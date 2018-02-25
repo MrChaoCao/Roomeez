98ecd74d60d2fccf5489cd70b6b2df33852d0d76
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-24 20:42:47 -0800

    Styled, but theres some funky positioning going on

--- client/src/components/group_index.jsx
+++ client/src/components/group_index.jsx
@@ -4,9 +4,11 @@ import { GroupIndexItem } from './group_index_item';
 class GroupIndex extends React.Component {
 
   render() {
-    if (Object.keys(this.props.groups).length > 0) {
+
+    if (Object.keys(this.props.groups).length > 0 && Object.keys(this.props.users).length > 0) {
       return (
         <div className="group-index">
+          <h1>Groups Looking for Roommates</h1>
           {Object.values(this.props.groups).map(group => {
             return <GroupIndexItem group={group} key={group._id} users={this.props.users}/>;
           })}
--- client/src/components/group_index_item.jsx
+++ client/src/components/group_index_item.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 export class GroupIndexItem extends React.Component {
   render() {
-    // console.log(this.props.users);
     return (
       <div className="group-index-item">
-        {this.props.group.name}
-        {this.props.group.members.map(member => {
-          return this.props.users[member].name;
-        })}
+        <h3>{this.props.group.name}</h3>
+        <div className="group-index-member-list">
+          {this.props.group.members.map(member => {
+            return <img alt={this.props.users[member].name}
+              src={this.props.users[member].image_url} key={member}/>;
+          })}
+        </div>
       </div>
     );
   }
