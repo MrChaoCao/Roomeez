30031cbf7df280eb9efb62d230696f0c11e3ca01
Adrian Horning <m.adrian.horning@gmail.com>
2018-02-25 18:42:03 -0800

    change

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
